e363bc8bee39d3cee2149af090cae2fafaa65e75
cherezov.vn <cherezov.vn@gazprom-neft.ru>
2024-06-13 16:38:04 +0500

    update

--- src/node-ui.ts
+++ src/node-ui.ts
@@ -123,12 +123,86 @@ export class NodeUI extends Svg {
 }
 
 const nui = new NodeUI(document.querySelector('.canvas'))
-nui.svg.innerHTML = '<g x="50" y="50" class="node" data-node="value" transform="translate(50,50)" id="ufv10Ga"><rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect><circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufv10Gc" data-to="ufv10Gg"></circle></g><g x="50" y="80" class="node" data-node="value" transform="translate(50,80)" id="ufv10Gd"><rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect><circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufv10Ge" data-to="ufv10Gh"></circle></g><g x="200" y="50" class="node selected" data-node="sum" transform="translate(161,58)" id="ufv10Gf"><rect x="0" y="0" width="80" height="40" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect><circle cx="0" cy="12" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufv10Gg" data-to="ufv10Gc"></circle><circle cx="0" cy="28" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufv10Gh" data-to="ufv10Ge"></circle><circle cx="80" cy="20" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufv10Gi"></circle></g><path class="link" stroke-linecap="round" d="M130 62 C 140.33333333333334 62, 150.66666666666666 70, 161 70" id="ufv10Gc-ufv10Gg"></path><path class="link" stroke-linecap="round" d="M130 92 C 140.33333333333334 92, 150.66666666666666 86, 161 86" id="ufv10Ge-ufv10Gh"></path>'
+// nui.svg.innerHTML = '<g x="50" y="50" class="node" data-node="value" transform="translate(50,50)" id="ufv10Ga"><rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect><circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufv10Gc" data-to="ufv10Gg"></circle></g><g x="50" y="80" class="node" data-node="value" transform="translate(50,80)" id="ufv10Gd"><rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect><circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufv10Ge" data-to="ufv10Gh"></circle></g><g x="200" y="50" class="node selected" data-node="sum" transform="translate(161,58)" id="ufv10Gf"><rect x="0" y="0" width="80" height="40" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect><circle cx="0" cy="12" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufv10Gg" data-to="ufv10Gc"></circle><circle cx="0" cy="28" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufv10Gh" data-to="ufv10Ge"></circle><circle cx="80" cy="20" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufv10Gi"></circle></g><path class="link" stroke-linecap="round" d="M130 62 C 140.33333333333334 62, 150.66666666666666 70, 161 70" id="ufv10Gc-ufv10Gg"></path><path class="link" stroke-linecap="round" d="M130 92 C 140.33333333333334 92, 150.66666666666666 86, 161 86" id="ufv10Ge-ufv10Gh"></path>'
+//language=HTML                   
+nui.svg.innerHTML = `
+    <g x="50" y="50" class="node" data-node="value" transform="translate(50,50)" id="ufzP1IF">
+        <rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect>
+        <circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufzP1II"
+                data-to="ufzP1IO ufzP1IW ufzP1IX"></circle>
+    </g>
+    <g x="50" y="80" class="node" data-node="value" transform="translate(50,80)" id="ufzP1IJ">
+        <rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect>
+        <circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufzP1IK"
+                data-to="ufzP1IM ufzP1IX ufzP1IO ufzP1IW"></circle>
+    </g>
+    <g x="200" y="50" class="node" data-node="sum" transform="translate(200,50)" id="ufzP1IL">
+        <rect x="0" y="0" width="80" height="40" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect>
+        <circle cx="0" cy="12" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufzP1IM"
+                data-to="ufzP1IK ufzP1IS ufzP1IU"></circle>
+        <circle cx="0" cy="28" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufzP1IO"
+                data-to="ufzP1II ufzP1IU ufzP1IS ufzP1IK"></circle>
+        <circle cx="80" cy="20" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufzP1IP"></circle>
+    </g>
+    <g x="50" y="150" class="node" data-node="value" transform="translate(45,140)" id="ufzP1IQ">
+        <rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect>
+        <circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufzP1IS"
+                data-to="ufzP1IW ufzP1IO ufzP1IM ufzP1IX"></circle>
+    </g>
+    <g x="50" y="180" class="node" data-node="value" transform="translate(50,180)" id="ufzP1IT">
+        <rect x="0" y="0" width="80" height="24" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect>
+        <circle cx="80" cy="12" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufzP1IU"
+                data-to="ufzP1IO ufzP1IX ufzP1IW ufzP1IM"></circle>
+    </g>
+    <g x="200" y="150" class="node selected" data-node="sum" transform="translate(194,159)" id="ufzP1IV">
+        <rect x="0" y="0" width="80" height="40" rx="2" stroke="#25334b" fill="#d7d7d7" class="handle"></rect>
+        <circle cx="0" cy="12" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufzP1IW"
+                data-to="ufzP1II ufzP1IS ufzP1IU ufzP1IK"></circle>
+        <circle cx="0" cy="28" r="4" stroke="#25334b" fill="#bcffd6" class="pin-in" id="ufzP1IX"
+                data-to="ufzP1IK ufzP1II ufzP1IU ufzP1IS"></circle>
+        <circle cx="80" cy="20" r="4" stroke="#25334b" fill="#ffc69a" class="pin-out" id="ufzP1IY"></circle>
+    </g>
+    <path class="link" stroke-linecap="round" d="M130 62 C 153.33333333333334 62, 176.66666666666666 78, 200 78"
+          id="ufzP1II-ufzP1IO"></path>
+    <path class="link" stroke-linecap="round" d="M200 62 C 176.66666666666666 62, 153.33333333333334 92, 130 92"
+          id="ufzP1IK-ufzP1IM"></path>
+    <path class="link" stroke-linecap="round"
+          d="M129.9999925825332 61.99998654259571 C 151.3333518240188 61.99998654259571, 172.6667110655044 171.00000847710498, 194.00007030698998 171.00000847710498"
+          id="ufzP1II-ufzP1IW"></path>
+    <path class="link" stroke-linecap="round" d="M130 192 C 153.33333333333334 192, 176.66666666666666 78, 200 78"
+          id="ufzP1IU-ufzP1IO"></path>
+    <path class="link" stroke-linecap="round"
+          d="M129.9999925825332 91.99996866120227 C 151.3333518240188 91.99996866120227, 172.6667110655044 186.99999894036182, 194.00007030698998 186.99999894036182"
+          id="ufzP1IK-ufzP1IX"></path>
+    <path class="link" stroke-linecap="round"
+          d="M125.0000245306228 151.99999083413013 C 148.00003978941186 151.99999083413013, 171.00005504820092 171.00000847710498, 194.00007030698998 171.00000847710498"
+          id="ufzP1IS-ufzP1IW"></path>
+    <path class="link" stroke-linecap="round" d="M125 152 C 150 152, 175 78, 200 78" id="ufzP1IS-ufzP1IO"></path>
+    <path class="link" stroke-linecap="round" d="M125 152 C 150 152, 175 62, 200 62" id="ufzP1IS-ufzP1IM"></path>
+    <path class="link" stroke-linecap="round"
+          d="M129.9999925825332 61.99998654259571 C 151.3333518240188 61.99998654259571, 172.6667110655044 186.99999894036182, 194.00007030698998 186.99999894036182"
+          id="ufzP1II-ufzP1IX"></path>
+    <path class="link" stroke-linecap="round"
+          d="M129.9999925825332 191.99996699227222 C 151.3333518240188 191.99996699227222, 172.6667110655044 186.99999894036182, 194.00007030698998 186.99999894036182"
+          id="ufzP1IU-ufzP1IX"></path>
+    <path class="link" stroke-linecap="round"
+          d="M129.9999925825332 191.99996699227222 C 151.3333518240188 191.99996699227222, 172.6667110655044 171.00000847710498, 194.00007030698998 171.00000847710498"
+          id="ufzP1IU-ufzP1IW"></path>
+    <path class="link" stroke-linecap="round"
+          d="M125.0000245306228 151.99999083413013 C 148.00003978941186 151.99999083413013, 171.00005504820092 186.99999894036182, 194.00007030698998 186.99999894036182"
+          id="ufzP1IS-ufzP1IX"></path>
+    <path class="link" stroke-linecap="round" d="M130 92 C 153.33333333333334 92, 176.66666666666666 78, 200 78"
+          id="ufzP1IK-ufzP1IO"></path>
+    <path class="link" stroke-linecap="round"
+          d="M129.9999925825332 91.99996866120227 C 151.3333518240188 91.99996866120227, 172.6667110655044 171.00000847710498, 194.00007030698998 171.00000847710498"
+          id="ufzP1IK-ufzP1IW"></path>
+    <path class="link" stroke-linecap="round" d="M130 192 C 153.33333333333334 192, 176.66666666666666 62, 200 62"
+          id="ufzP1IU-ufzP1IM"></path>`
 
 // nui.createNode(50, 50, 0, 1, 'value')
 // nui.createNode(50, 80, 0, 1, 'value')
 // nui.createNode(200, 50, 2, 1, 'sum')
-
+//
 // nui.createNode(50, 150, 0, 1, 'value')
 // nui.createNode(50, 180, 0, 1, 'value')
 // nui.createNode(200, 150, 2, 1, 'sum')
